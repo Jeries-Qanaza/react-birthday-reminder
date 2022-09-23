909f2686acfc7d7cfb64ca6eb623cd2a3c10d4cc
Your Name~ <you@example.com>
2022-09-23 13:04:38 +0300

    Updates

---
+++ precache-manifest.e3e06acac6bc306109468ca9fe233cd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b167e8a3fb144cf6d9e76683abd1703",
+    "url": "/react-birthday-reminder/index.html"
+  },
+  {
+    "revision": "b5e18bdd88924b2957e1",
+    "url": "/react-birthday-reminder/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/react-birthday-reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-birthday-reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e18bdd88924b2957e1",
+    "url": "/react-birthday-reminder/static/js/main.ce324959.chunk.js"
+  },
+  {
+    "revision": "ca18d9339a309e58c265",
+    "url": "/react-birthday-reminder/static/js/runtime-main.4fe2f5be.js"
+  }
+]);
